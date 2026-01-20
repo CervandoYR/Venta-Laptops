aa32d9f847768ec9bc280fd869e6bc281ebcbd8d
Cervandoyr <yactayocervando@gmail.com>
2026-01-20 18:47:30 -0500

    Cambios

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatPrice(price: number): string {
-  return new Intl.NumberFormat('es-MX', {
+  return new Intl.NumberFormat('es-PE', {
     style: 'currency',
-    currency: 'MXN',
+    currency: 'PEN',
   }).format(price)
 }
 
